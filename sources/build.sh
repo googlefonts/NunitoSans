e8674435f04f999d694e29d282ed840a2e3e9854
Emma Marichal <bonjour@emmamarichal.fr>
2023-02-15 14:54:50 +0100

    Tried a solution with a build script

---
+++ sources/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+echo "Build with gftools builder"
+gftools builder config.yaml
+
+echo "Slice into Roman and Italic VF (as Google Fonts currently doesn't support slnt or ital axes)"
+fonttools varLib.instancer "../fonts/variable/NunitoSans[YTLC,ital,opsz,wdth,wght].ttf" ital=0 wght=200:1000 --update-name-table -o "../fonts/variable//NunitoSans[YTLC,opsz,wdth,wght].ttf"
+fonttools varLib.instancer "../fonts/variable/NunitoSans[YTLC,ital,opsz,wdth,wght].ttf" ital=1 wght=200:1000 --update-name-table -o "../fonts/variable/NunitoSans-Italic[YTLC,opsz,wdth,wght].ttf"
+
+echo "Fix name ID 25"
+gftools gen-stat "../fonts/variable/NunitoSans-Italic[YTLC,opsz,wdth,wght].ttf" --axis-order 'wght' 'ital' --inplace
+
+echo "Fix Italic bits"
+python3 fix.py
+
+echo "Complete"
+
+
